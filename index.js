4feaa2ab70c3f650c731ad2e7225775f56111e2f
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-06-04 12:30:17 +0300

    feat(phonebook-backend): solve exercise 3.5, 3.6
    part-name: part 3a

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+
 const app = express();
+app.use(express.json());
 
 let persons = [
   {
@@ -62,6 +64,60 @@ app.delete("/api/persons/:id", (req, res) => {
     console.log(`Removed person with id=${id}`);
     res.status(204).end();
   }
+});
+
+// inspired from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function generateId() {
+  min = 100;
+  max = 10000;
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+
+  // ensure that the id is unique
+  let randomId = 1;
+  do {
+    randomId = Math.floor(
+      Math.random() * (maxFloored - minCeiled + 1) + minCeiled,
+    );
+  } while (persons.find((p) => p.id === randomId) !== undefined);
+
+  return randomId;
+}
+
+app.post("/api/persons", (req, res) => {
+  const { name, number } = req.body;
+
+  if (name === undefined) {
+    console.error(`cannot add person: name missing`);
+    return res.status(400).json({ error: "name missing" });
+  }
+
+  if (number === undefined) {
+    console.error(`cannot add person: number missing`);
+    return res.status(400).json({ error: "number missing" });
+  }
+
+  const personExists = persons.find(
+    (p) => p.name.trim().toLowerCase() === name.trim().toLowerCase(),
+  );
+  if (personExists !== undefined) {
+    console.error(
+      `cannot add person: person with the same name already exists`,
+    );
+    // from https://stackoverflow.com/questions/3290182/which-status-code-should-i-use-for-failed-validations-or-invalid-duplicates
+    return res.status(409).json({ error: "person already exists" });
+  }
+
+  const newPerson = {
+    id: generateId(),
+    name,
+    number,
+  };
+
+  persons = persons.concat(newPerson);
+  console.log(`"${name}" added to the persons database`);
+
+  res.status(201).json(newPerson);
 });
 
 const PORT = 3001;
