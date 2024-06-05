89fee8ce898833e79f8f28792020c82c9bc6628e
Mostafa Sabry <33330859+mrmostafaxd@users.noreply.github.com>
2024-06-05 19:58:56 +0300

    feat(phonebook-backend): complete exercise 3.8
    part-name: part 3a

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const morgan = require("morgan");
 
+morgan.token("data", (req) =>
+  req.method === "POST" ? JSON.stringify(req.body) : " ",
+);
+
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data"),
+);
 
 let persons = [
   {
